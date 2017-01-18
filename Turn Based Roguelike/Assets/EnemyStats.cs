51b339dd3a0928f908fb11a8a166830d07b0b0f6
rg-aeon <theoren75306@students.sbcsc.k12.in.us>
2017-01-18 07:38:03 -0500

    First Commit

---
+++ Turn Based Roguelike/Assets/EnemyStats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : MonoBehaviour {
+
+    new ArrayList enemyList;
+    new int enemyPick;
+    Random getRandom = new Random();
+
+    void RegEnemyPicker ()
+    {
+        enemyPick = getRandom.Next(0, 10);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Turn Based Roguelike/Assets/PlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerStats : MonoBehaviour {
+    int baseAttack = 10;
+    int baseDefense = 10;
+    new public float attack;
+    new public float defense;
+
+    new public float defIncreasePerLevel;
+    new public float attIncreasePerLevel;
+
+    void StatsChanges ()
+    {
+        defIncreasePerLevel = defense / 7;
+        attIncreasePerLevel = attack / 7;
+    }
+
+    void OnLevelUp ()
+    {
+        attack += attIncreasePerLevel;
+        defense += defIncreasePerLevel;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
