b357cbe1b0336cebb0f6e185ffaa8f846fccd90c
rg-aeon <theoren75306@students.sbcsc.k12.in.us>
2017-01-20 08:33:49 -0500

    Populated Enemy List and almost finished some RNG stuff

--- Turn Based Roguelike/Assets/EnemyStats.cs
+++ Turn Based Roguelike/Assets/EnemyStats.cs
@@ -3,18 +3,33 @@ using System.Collections;
 
 public class EnemyStats : MonoBehaviour {
 
-    ArrayList enemyList;
-    int enemyPick;
+    public string[] enemyList;
+    int enemyPick = Random.Range(0, 9);
     Random getRandom = new Random();
 
 
+
+    //int enemyPickInt = (int)enemyPick;
+    //return enemyPickInt;
+    //Debug.Log(enemyPickInt);
+
     void regEnemyPicker ()
     {
-        enemyPick = Random.Range(0, 10);
-        //int enemyPickInt = (int)enemyPick;
-        //return enemyPickInt;
-        //Debug.Log(enemyPickInt);
-
+        enemyList = new string[9];
+        enemyList[0] = "Wolf";
+        enemyList[1] = "Bear";
+        enemyList[2] = "Viking";
+        enemyList[3] = "Giant Snake";
+        enemyList[4] = "Farmer";
+        enemyList[5] = "Wild Dog";
+        enemyList[6] = "Lynx";
+        enemyList[7] = "Polar Bear";
+        enemyList[8] = "";
+        enemyList[9] = "";
+       foreach (string enemyList in enemyList)
+        {
+            
+        }
     }
 
 	// Use this for initialization
